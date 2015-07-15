6758b9c3a0edb8f71f356ebbeb77f88b9f33baf4
efimchick <thejerome@mail.ru>
2015-07-15 17:28:49 +0300

    Game basics

--- model/src/com/efimchick/battleships/model/game/MoveAction.java
+++ model/src/com/efimchick/battleships/model/game/MoveAction.java
@@ -1,5 +1,6 @@
 package com.efimchick.battleships.model.game;
 
+import com.efimchick.battleships.model.Wind;
 import com.efimchick.battleships.model.map.Cell;
 import com.efimchick.battleships.model.unit.Sailing;
 import com.efimchick.battleships.model.unit.Unit;
@@ -8,5 +9,6 @@ import com.efimchick.battleships.model.unit.Unit;
  * Created by efimchick on 03.10.14.
  */
 public class MoveAction<T extends Unit & Sailing> extends Action<T> {
+    Wind wind;
     Cell cell;
 }
--- model/src/com/efimchick/battleships/model/game/ShotAction.java
+++ model/src/com/efimchick/battleships/model/game/ShotAction.java
@@ -7,6 +7,7 @@ import com.efimchick.battleships.model.unit.Unit;
  * Created by efimchick on 03.10.14.
  */
 public class ShotAction<T extends Unit & Shooting> extends Action<T> {
-
+    int shots;
     boolean hit;
+    Unit taget;
 }
